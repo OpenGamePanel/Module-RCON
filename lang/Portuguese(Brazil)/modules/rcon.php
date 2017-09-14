94d37658aaa85b4153e7390ecef3e2f89b3f8c44
own3mall <own3mall@gmail.com>
2017-09-14 00:49:30 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Portuguese(Brazil)/modules/rcon.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('rcon_command_title', "Comandos RCON");
+define('has_sent_to', "foi enviado para");
+define('need_set_remote_pass', "necessário definir uma senha");
+define('before_sending_rcon_com', "antes de enviar comandos rcon");
+define('send_command', "Enviar comando");
+define('check-all', "Marcar todos");
+define('uncheck-all', "Desmarcar todos");
+?>
