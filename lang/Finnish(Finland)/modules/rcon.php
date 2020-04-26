c9208fa7a236d39ccf10ee0196c5b74a09941358
own3mall <own3mall@gmail.com>
2020-04-26 13:21:49 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Finnish(Finland)/modules/rcon.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_rcon_command_title', "RCON Commands");
+define('OGP_LANG_has_sent_to', "has sent to");
+define('OGP_LANG_need_set_remote_pass', "need set remote pass");
+define('OGP_LANG_before_sending_rcon_com', "before sending rcon com");
+define('OGP_LANG_send_command', "Send command");
+define('OGP_LANG_check-all', "Check all");
+define('OGP_LANG_uncheck-all', "Uncheck all");
+?>
