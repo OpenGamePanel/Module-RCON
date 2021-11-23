2d62aa7b1608cb372c4242a80f7dfdc88f412906
own3mall <own3mall@gmail.com>
2021-11-23 02:52:11 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Italian/modules/rcon.php
+++ lang/Italian/modules/rcon.php
@@ -22,11 +22,11 @@
  *
  */
 
-define('OGP_LANG_rcon_command_title', "RCON Commands");
+define('OGP_LANG_rcon_command_title', "Comandi RCON");
 define('OGP_LANG_has_sent_to', "has sent to");
 define('OGP_LANG_need_set_remote_pass', "need set remote pass");
 define('OGP_LANG_before_sending_rcon_com', "before sending rcon com");
-define('OGP_LANG_send_command', "Send command");
-define('OGP_LANG_check-all', "Check all");
-define('OGP_LANG_uncheck-all', "Uncheck all");
+define('OGP_LANG_send_command', "Invia comando");
+define('OGP_LANG_check-all', "Seleziona tutto");
+define('OGP_LANG_uncheck-all', "Deseleziona tutto");
 ?>
