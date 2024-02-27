72a9740d90cf71ee77c7cd230294a4f0ed609ee1
own3mall <own3mall@gmail.com>
2024-02-26 21:21:28 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Chinese(China)/modules/rcon.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_rcon_command_title', "RCON命令标题");
+define('OGP_LANG_has_sent_to', "已发送至");
+define('OGP_LANG_need_set_remote_pass', "需要设置远程RCON密码");
+define('OGP_LANG_before_sending_rcon_com', "发送RCON命令前");
+define('OGP_LANG_send_command', "发送命令");
+define('OGP_LANG_check-all', "全选");
+define('OGP_LANG_uncheck-all', "全不选");
+?>
